d2e6932428cfaad218f3ce53e42e89ceb827343d
Vikaskanwate <vikaskanwate32@gmail.com>
2025-02-06 00:49:25 +0530

    Create Array1.java

---
+++ Array/Array1.java
@@ -0,0 +1,35 @@
+package Array;
+public class Array1{
+    @SuppressWarnings("unused")
+    public static int length(int arr[]){
+        int len = 0;
+        for(int i:arr){
+            len++;
+        }
+        return len;
+    }
+    public static int max(int arr[]){
+        int a = Integer.MIN_VALUE;
+        for(int i = 0; i < length(arr);i++){
+            if(arr[i] > a){
+                a = arr[i];
+            }
+        }
+        return a;
+    }
+    public static int min(int arr[]){
+        int a = Integer.MAX_VALUE;
+        for(int i = 0; i < length(arr);i++){
+            if(arr[i] < a){
+                a = arr[i];
+            }
+        }
+        return a;
+    }
+    public static void main(String args[]){
+        int arr[] = {30,12,15,80};
+        length(arr);
+        System.out.println("Max value is :"+max(arr));
+        System.out.println("Min value is :"+min(arr));
+    }
+}
