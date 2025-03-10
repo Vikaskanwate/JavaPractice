ff76fa33121bf314219cb737c4bec1ad4fddc5fd
Vikaskanwate <vikaskanwate32@gmail.com>
2025-03-10 23:55:08 +0530

    MaxSubArray

---
+++ Array/MaxSubArray1.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class MaxSubArray1 {
+    public static void main(String[] args) {
+        int arr[] = {10,20,-10,50};
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for(var i : arr){
+            currSum += i;
+            maxSum  = Integer.max(currSum, maxSum);
+            if(currSum > 0){
+                currSum = 0;
+            }
+        }
+        System.out.println(maxSum);
+    }
+}
