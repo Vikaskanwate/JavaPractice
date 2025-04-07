572658267bec5accff9e7d19295a745d314b767e
Vikaskanwate <vikaskanwate32@gmail.com>
2025-04-07 22:27:20 +0530

    Update LeftRotate.java

--- Array/LeftRotate.java
+++ Array/LeftRotate.java
@@ -1,22 +1,30 @@
 package Array;
 
-public interface LeftRotate {
-    // left rotate by d places
-    public static void leftRotate(int arr[],int n,int d){
-        d = d % n;
-        int temp[]={};
-        for(int i = 0; i < d; i++){
-            temp[i]  = arr[i];
-        }
-        for(int i = d; i < n; i++){
-            arr[i - d] = arr[i];
-        }
+public class LeftRotate {
 
-        for(int i = n - d; i < n; i++){
-            arr[i] = temp[i-(n-1)];
+    // left rotate by d places
+    static public void Reverse(int arr[],int start,int end){
+        while (start <= end) {
+            int temp=arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
         }
     }
+
+    static public void leftRotate(int arr[],int n,int d){
+        LeftRotate.Reverse(arr, 0,d-1);
+        LeftRotate.Reverse(arr, d, n-1);
+        LeftRotate.Reverse(arr, 0, n-1);
+    }
     public static void main(String[] args) {
-        
+        int arr[] = {1,2,3,4,5,6,7};
+        int n = arr.length;
+        int d = 3;
+        leftRotate(arr, n, d);
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i] + " ");
+        }
     }
 }
