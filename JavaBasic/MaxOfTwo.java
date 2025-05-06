e0093005dfce70aeb1adb23df70650c0666f73fb
Vikaskanwate <vikaskanwate32@gmail.com>
2025-05-06 23:21:25 +0530

    practice

---
+++ JavaBasic/MaxOfTwo.java
@@ -0,0 +1,12 @@
+package JavaBasic;
+
+public class MaxOfTwo {
+    public static void main(String[] args) {
+        
+        int a = 10;
+        int b = 20;
+        String max = a > b  ? "number is max" : "num is min";
+        System.out.print(max); 
+    
+    }
+}
