b7b7d350d9d268f303a59562e1b24ce622c8e4c6
Vikaskanwate <vikaskanwate32@gmail.com>
2025-05-30 19:04:09 +0530

    practice

---
+++ JavaBasic/StringReverse.java
@@ -0,0 +1,12 @@
+package JavaBasic;
+
+public class StringReverse {
+    public static void main(String[] args) {
+        String str = "hello";
+        String rev = "";
+        for(int i = str.length()-1; i >= 0; i--){
+            rev += str.charAt(i);
+        }
+        System.out.println(rev);
+    }
+}
