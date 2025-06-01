10c3cc7a612a4714f6adbe5de9fc17fabc9ef182
Vikaskanwate <vikaskanwate32@gmail.com>
2025-06-01 23:31:08 +0530

    practice

---
+++ JavaBasic/DuplicateS.java
@@ -0,0 +1,25 @@
+package JavaBasic;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DuplicateS{
+    public static void main(String[] args) {
+        String str = "programming";
+        Map<Character,Integer> countCharAt = new HashMap<>();
+        for(char ch : str.toCharArray()){
+            if(countCharAt.containsKey(ch))
+                countCharAt.put(ch, countCharAt.get(ch) + 1);
+            else 
+                countCharAt.put(ch, 1);
+        }
+
+        
+        System.out.println("Duplicate characters ");
+        for(Map.Entry<Character,Integer> entry : countCharAt.entrySet()){
+            if(entry.getValue() > 1){
+                System.out.println(entry.getKey() + "-" + entry.getValue() + " times");
+            }
+        }
+    }
+}
