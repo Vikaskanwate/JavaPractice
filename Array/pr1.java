8576462ab469e0655167619a639a21a2878075ef
Vikaskanwate <vikaskanwate32@gmail.com>
2025-08-18 23:50:46 +0530

    practice

---
+++ Array/pr1.java
@@ -0,0 +1,18 @@
+package Array;
+public class pr1 {
+    public static void main(String[] args) {
+        // this is decimal 
+        int num = 123;
+        // this is octal
+        int num1 = 0123;
+        // this is hexadecimal
+        int num2 = 0x1234;
+        // binary
+        int num3 = 0b1010;
+        
+        System.out.println(num);
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+    }
+}
