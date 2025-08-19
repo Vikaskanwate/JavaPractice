2409b9012dab187b9a8db5c4b1e42912c60063f7
Vikaskanwate <vikaskanwate32@gmail.com>
2025-08-19 21:42:55 +0530

    practice

---
+++ employee/src/main/java/com/phoenix/employee/model/employee.java
@@ -0,0 +1,10 @@
+package com.phoenix.employee.model;
+
+public class employee {
+    private String id;
+    private String name;
+    private String department;
+    private String designation;
+    private Double price;
+
+}
