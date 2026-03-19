ca5ffb8d7029b60eb9a40ea67635032fb6e2be60
Vikaskanwate <vikaskanwate32@gmail.com>
2026-03-20 01:13:29 +0530

    practice

--- SpringBoot/crud/src/main/java/com/phoenix/crud/controller/StudentController.java
+++ SpringBoot/crud/src/main/java/com/phoenix/crud/controller/StudentController.java
@@ -1,9 +1,15 @@
 package com.phoenix.crud.controller;
 
+import com.phoenix.crud.model.Student;
 import com.phoenix.crud.service.StudentService;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/student")
 public class StudentController {
 
     private final StudentService studentService;
@@ -11,6 +17,20 @@ public class StudentController {
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
-    
+
+    @GetMapping
+    public ResponseEntity<List<Student>> getStudent(){
+            return new ResponseEntity<>(studentService.getAllStudents(), HttpStatus.OK);
+    }
+
+
+    @PostMapping
+    public ResponseEntity<String> saveStudent( @RequestBody Student student) {
+        if (studentService.addStudent(student)) {
+            return ResponseEntity.status(HttpStatus.CREATED).body("Data added");
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Not added");
+    }
+
 
 }
--- SpringBoot/crud/src/main/java/com/phoenix/crud/service/StudentService.java
+++ SpringBoot/crud/src/main/java/com/phoenix/crud/service/StudentService.java
@@ -1,16 +1,34 @@
 package com.phoenix.crud.service;
 
+import com.phoenix.crud.model.Student;
 import com.phoenix.crud.repository.StudentRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class StudentService {
 
     private final StudentRepository studentRepository;
 
-    public StudentService studentService(StudentRepository studentRepository){
-        this.studentRepository = studentRepository;x
+    public StudentService(StudentRepository studentRepository){
+        this.studentRepository = studentRepository;
     }
 
+    public List<Student> getAllStudents(){
+        return studentRepository.findAll();
+    }
+
+    public boolean addStudent(Student student){
+        if(student == null)
+            return false;
+        try{
+            studentRepository.save(student);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+
+    }
 
 }
