d309ad95c06dcddb53cc630dbe382cbe7fc433db
Vikaskanwate <vikaskanwate32@gmail.com>
2026-03-22 22:54:52 +0530

    practice

--- New/Basic/sum.java
+++ New/Basic/sum.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 public class sum {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter two numbers");
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter two numbers");
+            int a = sc.nextInt();
+            int b = sc.nextInt();
 
-        int sum = a + b;
+            int sum = a + b;
 
-        System.out.println("sum of a and b  is " + sum);
+            System.out.println("sum of a and b  is " + sum);
+        }
     }
 }
