ee30d45f70c81515d710b9837f6f2548603907c7
Vikaskanwate <vikaskanwate32@gmail.com>
2026-03-22 22:46:42 +0530

    practice

--- SpringBoot/crud/src/main/java/com/phoenix/crud/controller/StudentController.java
+++ SpringBoot/crud/src/main/java/com/phoenix/crud/controller/StudentController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/student")
+@CrossOrigin(origins="http://localhost:5173")
 public class StudentController {
 
     private final StudentService studentService;
