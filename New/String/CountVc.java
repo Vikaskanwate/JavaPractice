004e478fd8f581fff6716dd7f3081cbf8e5c07d1
Vikaskanwate <vikaskanwate32@gmail.com>
2026-04-06 02:12:33 +0530

    practice

---
+++ New/String/CountVc.java
@@ -0,0 +1,19 @@
+package New.String;
+public class CountVc {
+    public static void main(String[] args) {
+        String str = "Intermdiate Java Strings";
+        str = str.toLowerCase();
+
+        int vowels = 0, cons = 0;
+        for(char c : str.toCharArray()){
+            // if character not present it will return -1
+            if("aeiou".indexOf(c) != -1){
+                vowels++;
+            }else if(Character.isLetter(c)){
+                cons++;
+            }
+        }
+        System.out.println("Vowels: " + vowels + ", consonants: " + cons);
+    }    
+
+}
