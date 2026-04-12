8fde6395afc387e2851913674f5d17501706b198
Vikaskanwate <vikaskanwate32@gmail.com>
2026-04-12 20:01:40 +0530

    frequency of character in string

--- JavaBasic/Test_5.java
+++ JavaBasic/Test_5.java
@@ -4,20 +4,30 @@ public class Test_5 {
     public static void main(String[] args) {
         String str1 = "Hello World";
         String str = str1.toLowerCase().replaceAll(" ", "");
-        boolean [] freq = new boolean[256];
-        char ch [] = str.toCharArray();
-        System.out.println("Character Occurence: ");
-        for(int i = 0; i < ch.length;i++){
-            if(freq[i]) continue;
-            int c = 1;
-            for(int j = i + 1; j < ch.length; j++){
-                if(ch[i] == ch[j]){
-                    c++;
-                    freq[j] = true;
-                }
-            }
-            System.out.println(ch[i] + " : " +c);
-        }
+        // boolean [] freq = new boolean[256];
+        int freq[] = new int[26];
+        // char ch [] = str.toCharArray();
+        // System.out.println("Character Occurence: ");
+        // for(int i = 0; i < ch.length;i++){
+        //     if(freq[i]) continue;
+        //     int c = 1;
+        //     for(int j = i + 1; j < ch.length; j++){
+        //         if(ch[i] == ch[j]){
+        //             c++;
+        //             freq[j] = true;
+        //         }
+        //     }
+        //     System.out.println(ch[i] + " : " +c);
+        // }
+
+        // Optimized solution
         
+        for(char ch : str.toCharArray()){
+            freq[ch - 'a']++;
+        }
+        System.out.println("Character Occurence: ");
+        for(int i = 0; i < 26; i++){
+            if(freq[i] > 0) System.out.println((char)(i + 'a') + " : " + freq[i]);
+        }
     }
 }
