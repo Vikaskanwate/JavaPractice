f3984bfb29abe4f9ffc57265f913eea54512a171
Vikaskanwate <vikaskanwate32@gmail.com>
2026-04-17 00:19:59 +0530

    practice

--- JavaBasic/Test_7.java
+++ JavaBasic/Test_7.java
@@ -1,7 +1,13 @@
 package JavaBasic;
-// sliding window problem 
 public class Test_7 {
     public static void main(String[] args) {
+        String str = "Hello World!";
+        String [] words = str.split(" ");
+        for(int i = 0; i < words.length; i++){
+            words[i] = new StringBuilder(words[i]).reverse().toString();
+        }
+        String result = String.join(" ",words);
+        System.out.println(result);
         
     }
 }
