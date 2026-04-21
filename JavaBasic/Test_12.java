c8b722b9fe4e56f26ca05af201c92267eb6d94c9
Vikaskanwate <vikaskanwate32@gmail.com>
2026-04-21 07:44:29 +0530

    to find the longest streak

---
+++ JavaBasic/Test_12.java
@@ -0,0 +1,26 @@
+package JavaBasic;
+
+public class Test_12 {
+    public static void main(String[] args) {
+        String s = "beautiful";
+        int cv = 0, mv = 0,cc = 0,mc = 0;
+        char ch []= s.toCharArray();
+        for(int i = 0; i < ch.length; i++){
+            if(Character.isLetter(ch[i])){
+                if("aeiouAEIOU".indexOf(ch[i]) != -1){
+                    cv++;
+                    cc = 0;
+                    mv = Math.max(mv,cv);
+                }else{
+                    cc++;
+                    cv = 0;
+                    mc = Math.max(mc, cc);
+                }
+            }   
+
+        }
+        System.out.println("Longest vowel streak: " + mv);
+        System.out.println("Longest consonant streak: " + mc);
+        
+    }
+}
