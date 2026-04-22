106a8c8b9606e6a5c2b0e3bdaa3988dab859be9f
Vikaskanwate <vikaskanwate32@gmail.com>
2026-04-23 00:17:29 +0530

    focus

--- JavaBasic/Test_14.java
+++ JavaBasic/Test_14.java
@@ -3,20 +3,27 @@ package JavaBasic;
 // First non-repeating character: p
 public class Test_14 {
     public static void main(String[] args) {
-        String s = "programming";
+        String s = "aabb";
         
         String str = s.toLowerCase();
         int freq[] = new int[26]; 
         for(char ch : str.toCharArray()){
             freq[ch - 'a']++;
         }
-
+        // main part of the program simply loop through the array of char and search every element in the freq
+        //array if it equal to the 1 this way we can preserv the order as well 
         for(char ch : str.toCharArray()){
             if(freq[ch - 'a'] == 1){
                 System.out.println(ch);
                 return;
             }
         }
+        // for(char ch : str.toCharArray()){
+        //     if(freq[ch - 'a'] == 2){
+        //         System.out.println("No Reapting character found.");
+        //         return;
+        //     }
+        // }
         System.out.println("No Reapting character found.");
     }
 }
