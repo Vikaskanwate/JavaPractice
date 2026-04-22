6052b5c8c2360d7590ab67a5ab37fcbba6980142
Vikaskanwate <vikaskanwate32@gmail.com>
2026-04-22 23:00:31 +0530

    practice

---
+++ JavaBasic/Test_13.java
@@ -0,0 +1,26 @@
+package JavaBasic;
+// find logest vowel in string
+public class Test_13 {
+    public static void main(String[] args) {
+        String s = "beautiful";
+        String currentVowel = "" , longestVowel = "";
+        int vowelCount = 0,maxCount = 0;
+        for(char ch : s.toCharArray()){
+            if(Character.isLetter(ch)){
+                if("aeiouAEIOU".indexOf(ch) != -1){
+                    vowelCount++;
+                    currentVowel += ch;
+                    if (vowelCount > maxCount){
+                        maxCount = vowelCount;
+                        longestVowel = currentVowel;
+                    }
+                }else{
+                    currentVowel = "";
+                    vowelCount = 0;
+                }
+            }
+        }
+        System.out.println("streak of vowels: " + maxCount + " longest vowels :(" + longestVowel + ")");
+        
+    }
+}
