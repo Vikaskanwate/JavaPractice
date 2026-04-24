adfcaa3f5d1ca287f0c51673d9a736177bc69f9b
Vikaskanwate <vikaskanwate32@gmail.com>
2026-04-24 23:45:00 +0530

    practice

--- JavaBasic/Test_16.java
+++ JavaBasic/Test_16.java
@@ -1,6 +1,32 @@
 package JavaBasic;
 public class Test_16 {
+    static boolean isPalindrome(String s ){
+       String s1 = new StringBuilder(s).reverse().toString();
+       if(s1.equals(s)) return true;
+       return false;
+    }
     public static void main(String args[]){
-        
+        String s = "ac";
+        if(s.length() == 1){
+            System.out.println(s); 
+            return;
+        }
+        int count = 0;
+        int maxCount = 0;
+        String longestString = "";
+        for(int i = 0; i < s.length();i++){
+            for(int j = i + 1; j < s.length(); j++){
+                String str = s.substring(i, j);
+                if(isPalindrome(str)){
+                    count = str.length();
+                    if(count > maxCount){
+                        maxCount = count;
+                        longestString = str;
+                    }
+                }
+            }
+        }
+        System.out.println(longestString);
+    
     }
 }
