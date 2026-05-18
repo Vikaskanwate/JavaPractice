08c53b7c8cab8c9e2616a1bb6810fc1f81484fd9
Vikaskanwate <vikaskanwate32@gmail.com>
2026-05-18 23:04:36 +0530

    practice

--- JavaBasic/Test_33.java
+++ JavaBasic/Test_33.java
@@ -1,19 +1,27 @@
 package JavaBasic;
+
+import java.util.HashSet;
+
 // Q4. Find the First Duplicate Element
 // Input: {2, 5, 1, 2, 3, 5, 1}
 // Output: 2 
 public class Test_33 {
     public static void main(String[] args) {
-        int arr[] = {2, 5, 1, 2, 3, 5, 1};
-       
-        for(int i = 0;i < arr.length; i++){
-            for(int j = i+1; j < arr.length;j++){
-                if(arr[i] == arr[j]){
+        int arr[] = { 2, 5, 1, 5, 3, 5, 1 };
+        HashSet<Integer> duplicate = new HashSet<>();
+        for (int i = 0; i < arr.length; i++) {
+            // for (int j = i + 1; j < arr.length; j++) {
+            //     if (arr[i] == arr[j]) {
+            //         System.out.println(arr[i]);
+            //         return;
+            //     }
+            // }
+            if(duplicate.contains(arr[i])){
                 System.out.println(arr[i]);
                 return;
+            }else{
+                duplicate.add(arr[i]);
             }
-            }
-            
         }
     }
 }
